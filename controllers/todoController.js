a6093b5d5b028e79635f424a22ea3f289ef345c1
carlosBabel <carlos.babel@gmail.com>
2018-07-23 11:43:17 +0200

    cambio url

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -23,14 +23,14 @@ var urlencodeParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app) {
 
-    app.get('/todo', function(req, res) {
+    app.get('/', function(req, res) {
         //get data from mongodb and pass it to view
         Todo.find({}, function(err, data) {
             if (err) throw err;
             res.render('todo', {todos: data});
         });  
     });
-    app.post('/todo', urlencodeParser, function(req, res) {
+    app.post('/', urlencodeParser, function(req, res) {
         //get data from the view and add it to mongodb
         var newTodo = Todo(req.body).save(function(err, data) {
             if (err) throw err;
@@ -38,7 +38,7 @@ module.exports = function(app) {
         });
         
     });
-    app.delete('/todo/:tarea', function(req, res) {
+    app.delete('/:tarea', function(req, res) {
         //delete requested item from mongoDB
         Todo.find({tarea: req.params.tarea.replace(/\-/g, " ")}).remove(function(err, data) {
             if (err) throw err;
